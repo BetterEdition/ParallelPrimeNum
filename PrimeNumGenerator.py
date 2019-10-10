d59432f2f808971be055276b777b9cd264b383a0
betteredition <jesperenemark@hotmail.com>
2019-10-10 18:31:39 +0100

    isPrime still not working

--- PrimeNumGenerator.py
+++ PrimeNumGenerator.py
@@ -1,35 +1,45 @@
 import math
 import time
 
-input1 = 1
-input2 = 100000
 
 def IsPrime(num):
-        primeValid = True
         if num <= 1:
-                primeValid = False
+                return False
 
         if num == 2:
-                primeValid = True
+                return True
 
-        elif num % 2 == 0:
-                primeValid = False
+        if num % 2 == 0:
+                return False
         
+      
         i = 3
         
-        for i in range(i, int(math.floor(math.sqrt(num))), 2):
+        for i in range(i, i <= int(math.floor(math.sqrt(num))), 2):     
                         if num % i == 0:
-                                primeValid = False
-                                break
+                                return False
+                                
                         
-        return primeValid
+        return True
 
 def SequentialPrimeNumbers(num1, num2):
-        now = time.time
-        primes = 0
-        i = 0
-        for i in range(num1, i <= num2, 1):
-                if IsPrime(i):
-                        primes + 1
         
+        start_time = time.time()
+        
+        primes = 0
+       
+        for i in range(num1, num2 + 1, 1):
+                        print(i)
+                        if IsPrime(i):
+                                primes += 1
+        
+        elapsed_time = time.time() - start_time
+        print("Duration: " + "%0.10f" % elapsed_time)
+        print(primes)
         return primes
+
+IsPrime(9)
+def ParallelPrimeNumbers(num1, num2):
+        pass
+
+#SequentialPrimeNumbers(1, 10)
