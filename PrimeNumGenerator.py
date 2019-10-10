8c0d949c6b73058a85a67da2a5964591f35136cc
betteredition <jesperenemark@hotmail.com>
2019-10-10 15:22:51 +0100

    added some

--- PrimeNumGenerator.py
+++ PrimeNumGenerator.py
@@ -1,27 +1,35 @@
 import math
+import time
 
+input1 = 1
+input2 = 100000
 
 def IsPrime(num):
-        primeValid = False
+        primeValid = True
         if num <= 1:
                 primeValid = False
 
         if num == 2:
                 primeValid = True
 
-        if num % 2 == 0:
+        elif num % 2 == 0:
                 primeValid = False
+        
         i = 3
-
-        for i in i <= math.sqrt(num)
-        if num % i == 0:
-                        primeValid = True
-                        break
-        i += 2
+        
+        for i in range(i, int(math.floor(math.sqrt(num))), 2):
+                        if num % i == 0:
+                                primeValid = False
+                                break
+                        
         return primeValid
 
-def main():
-        print(IsPrime(3))
-
-
-main()
+def SequentialPrimeNumbers(num1, num2):
+        now = time.time
+        primes = 0
+        i = 0
+        for i in range(num1, i <= num2, 1):
+                if IsPrime(i):
+                        primes + 1
+        
+        return primes
